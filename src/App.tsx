f5aae1cbada414928b418aebadb578d9fe765553
AhmedMonem122 <ahmed.monem90@yahoo.com>
2025-12-18 00:40:43 +0200

    feat: add navbar component to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="bg-red-500 text-white">
-      <h1>Hello Vite + React</h1>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
 
