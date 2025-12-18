13de7479c5a9c8c4ef192fa4550670e93536fc5c
AhmedMonem122 <ahmed.monem90@yahoo.com>
2025-12-18 15:43:16 +0200

    feat: add add company component to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import AddCompany from "./components/AddCompany/AddCompany";
 import Navbar from "./components/Navbar/Navbar";
 import Sidebar from "./components/Sidebar/Sidebar";
 
@@ -7,6 +8,7 @@ function App() {
     <>
       <Navbar />
       <Sidebar />
+      <AddCompany />
     </>
   );
 }
